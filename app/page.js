a2b637da33c5044830aa3cc73e631096c14e4deb
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 07:59:35 -0700

    update

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
-import Image from "next/image";
-import Link from "next/link";
-
-export default function Page() {
+export default function Home() {
   return (
-    <main>
-      <h1>Shopping List</h1>
-      <Link href="./week-2">student-info</Link>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
+        <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
+        <div className="text-lg">
+          <ul>
+            {weekDemos.map((week) => (
+              <li key={week} className="hover:text-green-400 hover:underline">
+                <Link href={`/week-${week}`}>Week {week} Assignment</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
