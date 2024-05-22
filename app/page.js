e43dcd67fcfa10564fc7bb68c52c9bab746fda41
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 08:00:50 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,13 +4,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between text-sm">
         <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
         <div className="text-lg">
-          <ul>
-            {weekDemos.map((week) => (
-              <li key={week} className="hover:text-green-400 hover:underline">
-                <Link href={`/week-${week}`}>Week {week} Assignment</Link>
-              </li>
-            ))}
-          </ul>
+        <Link href={`/week-${week}`}>Week {week} Assignment</Link>
         </div>
       </div>
     </main>
