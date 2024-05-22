f472fdcf89c59a26d2cc4a0c4dcdd35f19986a76
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 07:42:31 -0700

    updated 5.22
    added the student info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from 'next/link';
+ 
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Katrina Keen Buenavista</p>
+            <p>
+                <Link href="">
+                    https://github.com
+                </Link>
+            </p>
+        </div>
+    );
+}
