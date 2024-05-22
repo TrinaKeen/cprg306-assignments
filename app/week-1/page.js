afce60b5b04bfc43699d9fb5feb3764d34f608cc
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 08:09:34 -0700

    updated assignment 5.22

---
+++ app/week-1/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import StudentGithub from "./student-github";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Please see below my github Link</h1>
+      <StudentGithub/>
+    </main>
+  );
+}
