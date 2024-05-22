4f8d123fa0a957021d29218b3801a1d9f34fdf01
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 08:10:20 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
       <div >
         <h1 className="text-4xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
         <div className="text-lg">
+        <li><Link className={linkstyles} href="./week-1">Assignment Week 1</Link></li>
         <li><Link className={linkstyles} href="./week-2">Assignment Week 2</Link></li>
         </div>
       </div>
