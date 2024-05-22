32cf9bcb2684f38aa7dc42bdf03185c287664f43
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 07:45:28 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <main>
       <h1>Shopping List</h1>
+      <li><Link href="./week-2">student-info</Link></li>
     </main>
   );
 }
