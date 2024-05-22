9fad109ebe0a931d0398f8114ef74a4ad16da3d0
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-05-22 08:11:20 -0700

    Update student-github.js

---
+++ app/week-1/student-github.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from 'next/link';
+ 
+export default function StudentGithub() {
+    return (
+        <div>
+            <p>Katrina Keen Buenavista</p>
+            <p>
+                <Link href="https://github.com/TrinaKeen/cprg306-assignments">
+                    https://github.com
+                </Link>
+            </p>
+        </div>
+    );
+}
