ed9e63eb0e8f0688606155eb7871c5a654d1dff2
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-06-03 22:50:36 -0700

    Update page.js

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ItemList from './item-list';
 import Item from './item';
 
-const Page = () => {
-  return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Shopping List</h1>
+export default function Page() {
+    return (
+    <main >
+      <h1 >Shopping List</h1>
       <ItemList />
     </main>
   );
