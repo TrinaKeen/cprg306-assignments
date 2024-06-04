3b4483f03966bdd83165a776670f719deb238260
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-06-03 23:29:16 -0700

    last update

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Item = ({ name, quantity, category }) => {
   return (
-    <li className="flex items-center justify-between p-4 bg-white rounded-lg shadow-md mb-2">
+    <li className="p-2 m-4 bg-slate-900 max-w-sm">
       <div>
-        <h2 className="text-lg font-semibold">{name}</h2>
-        <p className="text-sm text-gray-600">{category}</p>
-      </div>
-      <span className="text-blue-200 font-bold">{quantity}</span>
+        <h2 className="text-xl font-bold">{name}</h2>
+        <p className="text-sm text-blue-100" > Buy {quantity} in {category} </p><br></br>
+      </div> 
     </li>
+    
   );
 };
 
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -4,9 +4,10 @@ import Item from './item';
 
 export default function Page() {
     return (
-    <main >
-      <h1 >Shopping List</h1>
-      <ItemList />
-    </main>
+        <main className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold m-2">Shopping List</h1>
+        <br/>
+        <ItemList />
+      </main>
   );
 };
