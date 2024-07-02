32286e611eb44be63b559e8cb65b83b9eb3c954f
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-07-02 00:30:57 -0700

    Update item-list.js

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import Item from './Item';
+import Item from './item';
 
 export default function ItemList({ items }) {
     const [sortBy, setSortBy] = useState('name');
