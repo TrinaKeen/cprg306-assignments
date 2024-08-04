f940477e3f6d31db72ccdaca7f58be44cf902fd4
~YUNA~ <151793031+TrinaKeen@users.noreply.github.com>
2024-08-04 07:03:56 -0700

    Final Push.

--- app/week-10/page.js
+++ app/week-10/page.js
@@ -3,7 +3,7 @@
 import { useUserAuth } from './_utils/auth-context';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'; // Correct import for router
-import Link from 'next/link'; // Import Link for navigation
+import Link from 'next/link'; 
 
 const HomePage = () => {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
@@ -39,20 +39,27 @@ const HomePage = () => {
 
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h1>Shopping List App</h1>
+      <h1 className='text-3xl font-bold mb-4'>Shopping List App</h1>
 
       {!user ? (
-        <button onClick={handleSignIn} disabled={isLoading}>
+        <button 
+        className="w-half mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        onClick={handleSignIn} disabled={isLoading}>
           {isLoading ? 'Signing in...' : 'Sign in with GitHub'}
         </button>
       ) : (
         <div>
           <p>Signed in as {user.displayName || '~User~'} ({user.email})</p>
-          <button onClick={handleSignOut} disabled={isLoading}>
+          <button 
+          className="w-half mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          onClick={handleSignOut} disabled={isLoading}>
             {isLoading ? 'Signing out...' : 'Sign out'}
           </button>
           <br />
-          <Link href="./week-10/shopping-list">
+          <br></br>
+          <Link 
+          className="w-half mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          href="./week-10/shopping-list">
               Continue to your Shopping List
           </Link>
         </div>
--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -6,6 +6,7 @@ import { getItems, addItem } from '../_services/shopping-list-service'; // Impor
 import NewItem from './new-item';
 import ItemList from './item-list';
 import MealIdeas from './meal-ideas';
+import Link from 'next/link'; 
 
 const HomePage = () => {
   const { user } = useUserAuth(); // Get the user from auth context
@@ -51,6 +52,11 @@ const HomePage = () => {
     <div className="flex">
       <div className="w-1/4 p-10">
       <h2 class="text-3xl font-bold mb-4">Shopping List</h2>
+      <Link 
+      href="/"
+      className="w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+              Back to HomePage
+          </Link>
         <NewItem onAddItem={handleAddItem} />
         <ItemList items={items} onItemSelect={handleItemSelect} />
       </div>
